f9865406b671f23dc5292a28f6754bbd8bb38274
Nam4code <85899736+Nam4code@users.noreply.github.com>
2024-05-06 12:21:29 +0530

    Add files via upload

--- 3_detection.py
+++ 3_detection.py
@@ -1,28 +1,35 @@
+import os
 import numpy as np
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.densenet import preprocess_input
+from PIL import Image
 
-# Load the pre-trained VGG16 model
+# Load the trained model
 model = load_model('mango.keras')
 
-def predict_fruit_disease(image_path):
-    # Load and preprocess the image
-    img = image.load_img(image_path, target_size=(224, 224))
+# Define image dimensions
+img_width, img_height = 224, 224
+
+def preprocess_image(image_path):
+    img = image.load_img(image_path, target_size=(img_width, img_height))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.  # Rescale to [0,1]
+    img_array = preprocess_input(img_array)
+    return img_array
 
-    # Predict the class probabilities
-    predictions = model.predict(img_array)
-
-    # Convert probabilities to class labels
-    class_labels = ['Alternaria', 'Anthracnose', 'Black Mould Rot', 'Healthy', 'Stem end Rot']
-    predicted_class_index = np.argmax(predictions, axis=1)[0]
-    predicted_class = class_labels[predicted_class_index]
-
-    return predicted_class
+def classify_image(image_path):
+    preprocessed_img = preprocess_image(image_path)
+    prediction = model.predict(preprocessed_img)
+    predicted_class_index = np.argmax(prediction)
+    classes = ['Alternaria', 'Anthracnose', 'Black Mould Rot', 'Healthy', 'Stem end Rot']  # Update with your class labels
+    predicted_class = classes[predicted_class_index]
+    confidence = prediction[0][predicted_class_index]
+    return predicted_class, confidence
 
 # Example usage
-image_path = str(input("enter image path : "))
-predicted_label = predict_fruit_disease(image_path)
-print("Predicted class label:", predicted_label)
+image_path = str(input('enter image path : '))
+predicted_class, confidence = classify_image(image_path)
+print("Predicted Class:", predicted_class)
+print("Confidence:", confidence)
